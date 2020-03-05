10a275517190e0da9937d8989db01afdbf418b7d
kyd3snik <kartashovandrey73@gmail.com>
2020-03-05 08:48:47 +0300

    Initial commit

---
+++ src/test/java/com/example/travel/TravelApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
