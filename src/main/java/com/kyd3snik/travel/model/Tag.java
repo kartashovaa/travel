3a31aa86f208d64a75cfd2738b961852ca23c5f1
kyd3snik <kartashovandrey73@gmail.com>
2020-04-17 23:47:05 +0300

    use lombok annotation

--- src/main/java/com/kyd3snik/travel/model/Tag.java
+++ src/main/java/com/kyd3snik/travel/model/Tag.java
@@ -1,37 +1,20 @@
 package com.kyd3snik.travel.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Tag {
     @Id
     @GeneratedValue
     private long id;
     private String title;
-
-    public Tag() {
-    }
-
-    public Tag(long id, String title) {
-        this.id = id;
-        this.title = title;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
