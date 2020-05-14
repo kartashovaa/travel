ae91456904be3567a1259002ce319defe9e268f5
Alexandr-01 <derkachev01@list.ru>
2020-05-14 03:47:27 +0300

    Добавил лямбды и стримы. По стримам хз, пишет что игнорирую возвращаемое значение

--- src/main/java/com/kyd3snik/travel/services/ResortService.java
+++ src/main/java/com/kyd3snik/travel/services/ResortService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ResortService {
@@ -40,69 +41,34 @@ public class ResortService {
         TreeSet<Resort> res;
         switch (sort) {
             case COST_DOWN:
-                res = new TreeSet<>(new CostDownComparator());
+                res = new TreeSet<>((o1, o2) -> Long.compare(o2.getCost(), o1.getCost()));
                 break;
             case DURATION_UP:
-                res = new TreeSet<>(new DurationUpComparator());
+                res = new TreeSet<>(Comparator.comparingInt(Resort::getDurationInDays));
                 break;
             case DURATION_DOWN:
-                res = new TreeSet<>(new DurationDownComparator());
+                res = new TreeSet<>(Comparator.comparingLong(Resort::getCost);
                 break;
             case COST_UP:
             default:
-                res = new TreeSet<>(new CostUpComparator());
+                res = new TreeSet<>((o1, o2) -> Integer.compare(o2.getDurationInDays(), o1.getDurationInDays()));
         }
-        for (Resort resort : resortRepository.findAll()
-        ) {
-            if (resort.getCost() >= minCost && resort.getCost() <= maxCost && resort.getDurationInDays() >= minDuration
-                    && resort.getDurationInDays() <= maxDuration && startDate.after(resort.getStartDate())) {
-                res.add(resort);
-            }
-        }
+        res.stream()
+                .filter((resort) -> resort.getCost() >= minCost && resort.getCost() <= maxCost)
+                .filter((resort) -> resort.getDurationInDays() >= minDuration &&
+                        resort.getDurationInDays() <= maxDuration)
+                .filter((resort) -> startDate.after(resort.getStartDate()))
+                .collect(Collectors.toList());
         return res;
     }
 
     public Collection<Resort> search(int minCost, int maxCost, int minDuration, int maxDuration, Date startDate,
                                      SortType sort, List<Country> acceptableCountries) {
         TreeSet<Resort> res = new TreeSet<>(search(minCost, maxCost, minDuration, maxDuration, startDate, sort));
-        for (Resort resort : res
-        ) {
-            if (!acceptableCountries.contains(resort.getHotel().getCity().getCountry())) {
-                res.remove(resort);
-            }
-        }
+        res.stream()
+                .filter((resort) -> acceptableCountries.contains(resort.getHotel().getCity().getCountry()))
+                .collect(Collectors.toList());
         return res;
     }
 
-    private static class CostUpComparator implements Comparator<Resort> {
-
-        @Override
-        public int compare(Resort o1, Resort o2) {
-            return Long.compare(o1.getCost(), o2.getCost());
-        }
-    }
-
-    private static class CostDownComparator implements Comparator<Resort> {
-
-        @Override
-        public int compare(Resort o1, Resort o2) {
-            return Long.compare(o2.getCost(), o1.getCost());
-        }
-    }
-
-    private static class DurationUpComparator implements Comparator<Resort> {
-
-        @Override
-        public int compare(Resort o1, Resort o2) {
-            return Integer.compare(o1.getDurationInDays(), o2.getDurationInDays());
-        }
-    }
-
-    private static class DurationDownComparator implements Comparator<Resort> {
-
-        @Override
-        public int compare(Resort o1, Resort o2) {
-            return Integer.compare(o2.getDurationInDays(), o1.getDurationInDays());
-        }
-    }
 }
